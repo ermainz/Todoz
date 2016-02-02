dae05f07a24d63113670c65ce1a360d628b24982
Erik Mainz <erik.mainz@gmail.com>
2016-02-01 21:56:23 -0600

    Implemented prepending new item to list

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,16 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+function addItem(newItem) {
+  let newListItem = $('<li>').addClass('list-group-item').append(newItem);
+  $('.item-list').prepend(newListItem);
+}
+
+$( document ).ready(function() {
+
+    $('.add-item-button').on('click', function(ev) {
+      ev.preventDefault();
+      let newItem = $('.new-item-input').val();
+      addItem(newItem);
+    });
+
+});
