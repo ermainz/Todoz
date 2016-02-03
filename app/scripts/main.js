7819fd945aa53161ae49d204094620509542504c
Erik Mainz <erik.mainz@gmail.com>
2016-02-02 21:52:29 -0600

    Added done button to list rows
    Add button to list rows. The button doesn't do anything yet.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,18 @@
 'use strict';
 
+function buildOkGlyphicon() {
+  return $('<span>').addClass('glyphicon').addClass('glyphicon-ok');
+}
+
+function buidlDoneButton() {
+  let button = $('<button>').addClass('pull-right').addClass('btn').addClass('btn-xs').addClass('btn-success');
+  button.append(buildOkGlyphicon());
+  return button;
+}
+
 function addItem(newItem) {
   let newListItem = $('<li>').addClass('list-group-item').append(newItem);
+  newListItem.append(buidlDoneButton());
   $('.item-list').prepend(newListItem);
 }
 
